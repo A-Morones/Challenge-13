9a82b59f9953b1eb457d5a372083757786feaaa3
Adalberto Morones <adalbertomorones@yahoo.com>
2024-08-12 10:57:38 -0500

    made changes to models files.

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,7 +6,15 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // define 
+    type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+  },
+    category_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
   {
     sequelize,
